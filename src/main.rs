7020a0bcd3c4db817c6b08579ca90f2b3dc5c4ed
Michal Maruska <mmaruska@gmail.com>
2025-06-17 10:26:36 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+
+// walk the hierarchy
+// - assemble list of segments/sums.
+// - graph, toposort
+
+//
+// get list of segments
+use git2::Repository;
+use clap::Parser;
+// use std::path::PathBuf;
+
+// error: cannot find derive macro `Parser` in this scope
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    directory: Option<String>
+}
+
+
+const GLOB_REFS_BASES: &str = "refs/base/*";
+
+///
+// fn refsWithPrefixIter(iterator storer.ReferenceIter, prefix string) storer.ReferenceIter {
+
+pub trait NodeExpander {
+    fn NodeIdentity(&self) -> &str; // same lifetime
+
+    // not object-safe:
+    // so Self is not ok, but NodeExpander is ?
+    fn NodePrepare(&self) -> &dyn NodeExpander; // upgrade itself? or what
+    fn NodeChildren(&self) -> [&dyn NodeExpander]; // owned!
+}
+
+
+
+fn main() {
+    let cli = Cli::parse();
+
+    let repo = match Repository::open(cli.directory.unwrap_or(".".to_string())) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+    // `git2::Repository` cannot be formatted with the default formatter
+    // `git2::Repository` cannot be formatted using `{:?}` because it doesn't implement `std::fmt::Debug`
+    println!("{:?}", repo.namespace());
+    let head = repo.head();
+
+    if let Ok(refsi) = repo.references_glob (&GLOB_REFS_BASES) {
+        for reference in refsi {
+            println!("{:?}", reference.unwrap().name());
+        }
+    }
+
+
+
+    // let msg = repo.message();
+    // println!("{:?}", &head);
+}
