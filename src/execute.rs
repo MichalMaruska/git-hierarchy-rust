4ca5ea9e4ca1b224e2873e4245f7cb2481cbf657
Michal Maruska <mmaruska@gmail.com>
2025-06-30 17:55:41 +0200

    execute: git_run(), return status

---
+++ src/execute.rs
@@ -0,0 +1,21 @@
+pub use std::process::{Command,ExitStatus};
+use git2::Repository;
+
+#[allow(unused)]
+use log::{self,info,warn,error,debug};
+
+/// Invoke git with the given CLI arguments. In the directory of the @repository.
+pub fn git_run(repository: &Repository, cmd_line: &[&str]) -> ExitStatus {
+    let mut command = Command::new("git");
+    command.args(cmd_line);
+    command.current_dir(repository.workdir().unwrap());
+    debug!("must cd into {}", repository.workdir().unwrap().display());
+    warn!("git-run: {}", cmd_line.join(" "));
+
+    let child = command.spawn().expect("git command failed to start");
+    let output = child.wait_with_output().expect("Failed to wait on git");
+
+    dbg!(output.status);
+    output.status
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod utils;
 pub mod git_hierarchy;
 pub mod graph;
 pub mod permutation;
+pub mod execute;
