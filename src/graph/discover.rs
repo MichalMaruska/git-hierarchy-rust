ec3e44ce2e2f83d0aa2acd353f2b06d01e1ae6fa
Michal Maruska <mmaruska@gmail.com>
2025-08-20 16:40:12 +0200

    discover: drop discover_graph()
    Disable funky loop, so drop log -- workaround error
    
    let this = vertices.get_mut(current).unwrap();
       |                    ^^^^^^^^ `vertices` was mutably borrowed here in the previous iteration of the loop

--- src/graph/discover.rs
+++ src/graph/discover.rs
@@ -1,6 +1,3 @@
-use log::{self,info};
-use std::collections::HashMap;
-
 use crate::graph::Graph;
 
 pub trait NodeExpander {
@@ -11,51 +8,4 @@ pub trait NodeExpander {
     fn node_prepare(&mut self); // -> &dyn NodeExpander; // upgrade itself? or what
     fn node_children(&self) -> Vec<Box<dyn NodeExpander + '_>>; // owned!
 
-}
-
-pub fn discover_graph(start: Vec<Box<dyn NodeExpander>>) -> (Graph, Vec<Box<dyn NodeExpander>>)
-{
-    let mut graph = Graph::new();
-    graph.add_vertices(start.len());
-
-    // this will be produced, we could use the start Vector! todo!
-    let mut vertices = start;
-
-    // |start|.....
-    // |------|-------------|......|  vertices
-    //        ^ reader      ^appender
-    //
-
-    // mmc: it's a set!  so maps into the vector indices
-    let mut known : HashMap<String, usize> = HashMap::new();
-
-    let mut current = 0;
-    loop {
-        let this = vertices.get_mut(current).unwrap();
-        info!("visiting node {} {}", current, this.node_identity());
-
-        this.node_prepare();
-
-        let children =  this.node_children();
-
-        for child in children {
-            if let Some(found) = known.get(child.node_identity()) {
-                info!("adding edge to already known node {}", child.node_identity());
-                graph.add_edge(current, *found);
-            } else {
-                vertices.push(child);
-                let new_index = vertices.len() - 1;
-                graph.add_vertices(new_index);
-                graph.add_edge(current, new_index);
-                info!("adding new vertex: child to the list {} {}", &vertices[new_index].node_identity(), new_index);
-
-                known.insert(vertices[new_index].node_identity().to_string(), new_index);
-            }
-        }
-
-        current += 1;
-        if current == vertices.len() {break}
-    }
-
-    return (graph, vertices);
 }
