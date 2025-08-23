044462c63781aa4f0dcc9f838f75f6c315a80323
Michal Maruska <mmaruska@gmail.com>
2025-08-23 07:00:35 +0200

    git-walk-down: fixes?

--- src/bin/git-walk-down.rs
+++ src/bin/git-walk-down.rs
@@ -4,16 +4,10 @@
 // - clone
 // - replaceInHierarchy ...the base from->to, mapping
 
-//
-// get list of segments
-use git2::{Repository,Reference,Error};
 use clap::Parser;
-// use std::error::Error;
-use log::{self,info,error};
-use stderrlog::LogLevelNum;
-// use tracing::{Level, event, instrument};
 
-use ::git_hierarchy::base::{get_repository,set_repository,unset_repository};
+use git2::{Repository,};
+use ::git_hierarchy::base::{set_repository,unset_repository};
 
 use ::git_hierarchy::*;
 /*
@@ -29,7 +23,6 @@ use graph::discover::NodeExpander;
 use graph::topology_sort::topological_sort;
 
 // use std::path::PathBuf;
-use tracing::debug;
 use tracing_subscriber;
 
 // error: cannot find derive macro `Parser` in this scope
@@ -52,10 +45,11 @@ fn main() {
     set_repository(repo);
 
     // load one Segment:
-    let mut root = GitHierarchy::Name(cli.root_reference.unwrap_or("mmc".to_string()));
+    let root = GitHierarchy::Name(cli.root_reference.unwrap());
     println!("root is {}", root.node_identity());
     let (graph, vertices ) =
         graph::discover::discover_graph(vec!(Box::new(root)));
+
 
     let order = graph.toposort();
     for i in &order {
