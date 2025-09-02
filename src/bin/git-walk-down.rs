18589090848f96cb92119fd2539a1a9aa8efc592
Michal Maruska <mmaruska@gmail.com>
2025-09-02 22:36:37 +0200

    walk-down: with a callback process_node()

--- src/bin/git-walk-down.rs
+++ src/bin/git-walk-down.rs
@@ -6,6 +6,8 @@
 
 use clap::Parser;
 use git2::{Repository,};
+
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 use ::git_hierarchy::utils::init_tracing;
@@ -20,7 +22,82 @@ use ::git_hierarchy::graph::discover::NodeExpander;
 use ::git_hierarchy::graph::discover_pet::{find_hierarchy};
 
 #[allow(unused)]
+use ::git_hierarchy::git_hierarchy::{GitHierarchy, Segment, Sum, load};
+
+#[allow(unused)]
 use tracing::{debug, info};
+
+fn process_node<'repo>(
+    repository: &'repo Repository,
+    node: &GitHierarchy<'repo>,
+    _object_map: &HashMap<String, GitHierarchy<'repo>>, // _remapped : HashMap<String, String>,
+) {
+    println!(
+        "{:?}",
+        // object_map.get(&v).unwrap()
+        node.node_identity(),
+        // object_map
+        // graph.node_weight(hash_to_graph.get(node).unwrap().clone()).unwrap()
+    );
+
+    match node {
+        GitHierarchy::Name(_n) => {
+            panic!();
+        }
+        GitHierarchy::Reference(r) => {
+            println!("a ref");
+        }
+        GitHierarchy::Segment(segment) => {
+            let base = segment.base(repository);
+            let start = &segment._start;
+            // start == base.peel_to_commit().unwrap())
+
+            // target
+            let state;
+            if base.peel_to_commit().unwrap().id() == start.target().unwrap() {
+                state = "up-to-date";
+            } else {
+                state = "need-rebase";
+            }
+            println!(
+                "segment {:?} on , {:?}",
+                base.name().unwrap(),
+                state // base.peel_to_commit().unwrap().id(),
+            );
+        }
+        GitHierarchy::Sum(sum) => {
+            let summands = sum.summands(repository);
+
+            println!("a sum of: ");
+            for s in &summands {
+                println!("{}", s.node_identity());
+            }
+        }
+    }
+}
+
+fn walk_down<F>(repository: &Repository, root: &str, mut process: F)
+where
+    F: for<'repo, 'a> FnMut(
+        &'repo git2::Repository,
+        &GitHierarchy<'repo>,
+        &'a HashMap<String, GitHierarchy<'repo>>,
+    ) -> (), // F : FnMut(&Repository, GitHierarchy,
+             //          &HashMap<String, GitHierarchy>) -> ()
+{
+    let (
+        object_map,     // String -> GitHierarchy
+        _hash_to_graph, // stable graph:  String -> index ?
+        _graph,         // index -> String?
+        discovery_order,
+    ) = find_hierarchy(repository, root.to_owned());
+
+    // convert the gh objects?
+    for v in discovery_order {
+        let vertex = object_map.get(&v).unwrap();
+        process(repository, vertex, &object_map);
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
