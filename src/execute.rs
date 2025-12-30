13e4e73b509706b4b7971260adeb3c3f0eec7412
Michal Maruska <mmaruska@gmail.com>
2025-12-30 17:27:28 +0100

    git_run: return custom Error

--- src/execute.rs
+++ src/execute.rs
@@ -4,11 +4,18 @@ pub use std::process::{Command, ExitStatus};
 #[allow(unused)]
 use tracing::{debug, info, warn, error};
 
+
+pub enum Error {
+    NoWorkDir,
+    ProcessError(std::io::Error),
+}
+
 /// Invoke git with the given CLI arguments. In the directory of the @repository.
-pub fn git_run(repository: &Repository, cmd_line: &[&str]) -> ExitStatus {
+pub fn git_run(repository: &Repository, cmd_line: &[&str]) -> Result<ExitStatus, Error> {
     let mut command = Command::new("git");
     command.args(cmd_line);
-    command.current_dir(repository.workdir().unwrap());
+
+    command.current_dir(repository.workdir().ok_or(Error::NoWorkDir)?);
     debug!("must cd into {}", repository.workdir().unwrap().display());
     warn!("git-run: {}", cmd_line.join(" "));
 
@@ -16,5 +23,5 @@ pub fn git_run(repository: &Repository, cmd_line: &[&str]) -> ExitStatus {
     let output = child.wait_with_output().expect("Failed to wait on git");
 
     dbg!(output.status);
-    output.status
+    Ok(output.status)
 }
