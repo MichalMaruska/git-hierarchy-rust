c06873151d5ae9000679d99a79b70737fb4a5666
Michal Maruska <mmaruska@gmail.com>
2026-01-14 12:33:04 +0100

    git-walk-down: explain that Root must be written when doing replace

--- src/bin/git-walk-down.rs
+++ src/bin/git-walk-down.rs
@@ -299,11 +299,12 @@ fn main() {
     let repository = open_repository(cli.directory.as_ref()).unwrap();
     if !cli.replace.is_empty() {
         if cli.replace.len() != 2 {
-            panic!("--rename takes 2 parameters");
+            panic!("--replace takes 2 parameters");
         }
         // also, in this case I don't start *implicitly* by HEAD.
         if cli.root_reference.is_none() {
-            panic!("when --rename is used, the top must be stated")
+            eprintln!("when --replace is used, the top must be stated ... {}", current_branch(&repository));
+            std::process::exit(1);
         }
     }
 
