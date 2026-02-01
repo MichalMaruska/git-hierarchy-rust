7121a038c0b0cb5369c920ba9df1df07214aa8da
Michal Maruska <mmaruska@gmail.com>
2026-02-01 11:42:53 +0100

    squash! rebase/main: don't panic, just exit, and report error

--- src/bin/rebase/main.rs
+++ src/bin/rebase/main.rs
@@ -503,14 +503,11 @@ fn main() {
         }
     }
 
-    match rebase_tree(&repository, root.node_identity().to_owned(), !cli.no_fetch,
-                      &cli.ignore,
-                      &cli.skip) {
-        Err(e) => {
-            eprintln!("Failed: {:?}", e); // RebaseError
-            exit(-1);
-        }
-        Ok(_) => {
-        }
+    if let Err(e) = rebase_tree(&repository, root.node_identity().to_owned(), !cli.no_fetch,
+                                &cli.ignore,
+                                &cli.skip)
+    {
+        eprintln!("Failed: {:?}", e); // RebaseError
+        exit(-1);
     }
 }
